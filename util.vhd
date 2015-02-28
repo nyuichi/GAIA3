1e0d3f66c9e8a681b0d119048f02003e22f1dcf5
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-02-28 15:24:06 +0900

    add stdv2str

--- util.vhd
+++ util.vhd
@@ -7,6 +7,7 @@ package util is
   function to_std_logic(b : boolean) return std_logic;
   function repeat(B: std_logic; N: natural) return std_logic_vector;
   function normalize_fzero(a : std_logic_vector(31 downto 0)) return std_logic_vector;
+  function stdv2str(vec : std_logic_vector) return string;
 
 end package;
 
@@ -43,5 +44,32 @@ package body util is
     end if;
     return result;
   end function;
+
+  function stdv2str(vec:std_logic_vector) return string is
+    variable str: string(vec'left+1 downto 1);
+  begin
+    for i in vec'reverse_range loop
+      if(vec(i)='U') then
+        str(i+1):='U';
+      elsif(vec(i)='X') then
+        str(i+1):='X';
+      elsif(vec(i)='0') then
+        str(i+1):='0';
+      elsif(vec(i)='1') then
+        str(i+1):='1';
+      elsif(vec(i)='Z') then
+        str(i+1):='Z';
+      elsif(vec(i)='W') then
+        str(i+1):='W';
+      elsif(vec(i)='L') then
+        str(i+1):='L';
+      elsif(vec(i)='H') then
+        str(i+1):='H';
+      else
+        str(i+1):='-';
+      end if;
+    end loop;
+    return str;
+  end;
 
 end util;
